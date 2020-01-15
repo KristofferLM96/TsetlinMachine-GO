3edfe6c04577a421b629d27166bbb83ebb3b8036
Storebo <tstorebo@gmail.com>
2020-01-15 13:18:35 +0100

    Fixed cTM clauses, added option to avoid writing clauses

--- TsetlinGo.py
+++ TsetlinGo.py
@@ -5,39 +5,68 @@ import csv
 from time import time
 import time as stime
 
-def StartMachine(clauses,epoch,Threshold,S,inndata,dim,machine,Window_X,Window_Y,Shape_X,Shape_Y,Shape_Z,Name):
-    timestr = stime.strftime("%m%d-%H%M")
-    results = open("Results/"+Name+"/"+machine+"/"+machine+dim+timestr+".csv",'a')
-    resultclauses = open("Results/"+Name+"/"+machine+"/"+machine+dim+timestr+"clauses1.csv",'a')
 
-    def GetOutput(tm,tm_class,clause):
+def StartMachine(clauses, epoch, Threshold, S, inndata, dim, machine, Window_X, Window_Y, Shape_X, Shape_Y, Shape_Z,
+                 Name, Write_Clauses):
+    timestr = stime.strftime("%m%d-%H%M")
+    results = open("Results/" + Name + "/" + machine + "/" + machine + dim + timestr + ".csv", 'a')
+
+    def TMGetOutput(tm, tm_class, clause):
         output = []
-        for i in range(84*2):
-            outputbit = tm.ta_action(tm_class,clause,i)
+        for i in range(Shape_X * Shape_Y * 4):
+            outputbit = tm.ta_action(tm_class, clause, i)
             output.append(outputbit)
         return output
-    def Align(tm,tm_class,clause):
-        output = GetOutput(tm,tm_class,clause)
-        nonNegated = output[:int(len(output)/2)]
-        negated = output[int(len(output)/2):]
-        xbit = (nonNegated[:int(len(nonNegated)/2)])
-        obit = (nonNegated[int(len(nonNegated)/2):])
-        nxbit = (negated[:int(len(negated)/2)])
-        nobit = (negated[int(len(negated)/2):])
-        board=[]
-        for i in range(42):
-            resultclauses.write(str(xbit[i])+str(obit[i])+str(nxbit[i])+str(nobit[i]))
-            if i < 41:
+
+    def Align(tm, tm_class, clause):
+        output = ""
+        if machine == "TM":
+            output = TMGetOutput(tm, tm_class, clause)
+        if machine == "cTM":
+            output = cTMGetOutput(tm, tm_class, clause)
+        nonNegated = output[:int(len(output) / 2)]
+        negated = output[int(len(output) / 2):]
+        xbit = (nonNegated[:int(len(nonNegated) / 2)])
+        obit = (nonNegated[int(len(nonNegated) / 2):])
+        nxbit = (negated[:int(len(negated) / 2)])
+        nobit = (negated[int(len(negated) / 2):])
+        board = []
+        for i in range(Shape_X * Shape_Y):
+            resultclauses.write(str(xbit[i]) + str(obit[i]) + str(nxbit[i]) + str(nobit[i]))
+            if i < Shape_X * Shape_Y - 1:
                 resultclauses.write(",")
             else:
                 resultclauses.write("\n")
 
-    def PrintClass(Ts,Class,clauses):
+    offset_y = Shape_Y - Window_Y
+    offset_x = Shape_X - Window_X
+
+    def cTMGetOutput(tm, tm_class, clause):
+        output = []
+        xyz_id_old = 0
+        for y in range(Window_Y):
+            for x in range(Window_X):
+                for z in range(Shape_Z):
+                    xyz_id = offset_y + offset_x + y * Shape_X * 2 + x * 2 + z
+                    outputbit = tm.ta_action(tm_class, clause, xyz_id)
+                    output.append(outputbit)
+                    xyz_id_old = xyz_id + 1
+        output = cTMGetOutNegated(tm, tm_class, clause, xyz_id_old, output)
+        return output
+
+    def cTMGetOutNegated(tm, tm_class, clause, xyz_id_old, output):
+        for y in range(Window_Y):
+            for x in range(Window_X):
+                for z in range(Shape_Z):
+                    xyz_id = xyz_id_old + offset_y + offset_x + y * Shape_X * 2 + x * 2 + z
+                    outputbit = tm.ta_action(tm_class, clause, xyz_id)
+                    output.append(outputbit)
+        return output
+
+    def PrintClass(Ts, Class, clauses):
         for i in range(clauses):
-            Align(Ts,Class,i)
-            #resultclauses.writelines(clausesres)
-            #print(clausesres)
-            #PrintClause(action)
+            Align(Ts, Class, i)
+
     ecount = 0
     counter = 0
     if machine == "TM":
@@ -46,42 +75,44 @@ def StartMachine(clauses,epoch,Threshold,S,inndata,dim,machine,Window_X,Window_Y
         results.write(
             "MultiClassConvolutionalTsetlinMachine2D,Parallel,")
     while ecount < epoch:
-        results.write("Epoch"+str(ecount+1)+",")
-        ecount+=1
+        results.write("Epoch" + str(ecount + 1) + ",")
+        ecount += 1
     results.write("\n")
 
-    if machine =="TM": results.write(
+    if machine == "TM": results.write(
         "Settings:\nClauses:,%.1f\nThreshold:,%.1f\nS:,%.1f\n" % (clauses, Threshold, S))
-    if machine =="cTM": results.write(
+    if machine == "cTM": results.write(
         "Settings:\nClauses:,%.1f\nThreshold:,%.1f\nS:,%.1f\nWindow_X:,%.1f\nWindow_Y:,%.1f\nShape_X:,%.1f\nShape_Y:,%.1f\nShape_Z:,%.1f\n" % (
-        clauses, Threshold, S, Window_X, Window_Y, Shape_X, Shape_Y, Shape_Z))
+            clauses, Threshold, S, Window_X, Window_Y, Shape_X, Shape_Y, Shape_Z))
 
     while counter < 10:
         numb = str(counter)
-        global X_train,Y_train,X_test,Y_test,m
-        train_data = np.loadtxt("dataset/" + dim + inndata + numb + "train", delimiter=",")
-        test_data = np.loadtxt("dataset/" + dim + inndata + numb + "test", delimiter=",")
-        #tm.fit(X_train, Y_train, epochs=200)
+        global X_train, Y_train, X_test, Y_test, m
+        train_data = np.loadtxt("Data/" + dim + inndata + numb + "train", delimiter=",")
+        test_data = np.loadtxt("Data/" + dim + inndata + numb + "test", delimiter=",")
         if machine == "TM":
             X_train = train_data[:, 0:-1]
             Y_train = train_data[:, -1]
             X_test = test_data[:, 0:-1]
             Y_test = test_data[:, -1]
-            m = MultiClassTsetlinMachine(clauses, Threshold, S, boost_true_positive_feedback=1, weighted_clauses=True)
+            m = MultiClassTsetlinMachine(clauses, Threshold, S, boost_true_positive_feedback=0, weighted_clauses=True)
             print("-------------------------------------------------------------------------------------------")
-            print("MultiClassTsetlinMachine using %s%s%s writen to file %s.csv\n" %(dim,inndata,numb,timestr))
+            print("MultiClassTsetlinMachine using %s%s%s writen to file %s.csv\n" % (dim, inndata, numb, timestr))
             print("Settings: Clauses: %.1f Threshold: %.1f S: %.1f\n" % (clauses, Threshold, S))
         if machine == "cTM":
             X_train = train_data[:, 0:-1].reshape(train_data.shape[0], Shape_X, Shape_Y, Shape_Z)
             Y_train = train_data[:, -1]
             X_test = test_data[:, 0:-1].reshape(test_data.shape[0], Shape_X, Shape_Y, Shape_Z)
             Y_test = test_data[:, -1]
-            m = MultiClassConvolutionalTsetlinMachine2D(clauses, Threshold, S, (Window_X, Window_Y),boost_true_positive_feedback=1, weighted_clauses=True)
+            m = MultiClassConvolutionalTsetlinMachine2D(clauses, Threshold, S, (Window_X, Window_Y),
+                                                        boost_true_positive_feedback=0, weighted_clauses=True)
             print("-------------------------------------------------------------------------------------------")
-            print("MultiClassConvolutionalTsetlinMachine2D using %s %s writen to file %s.csv (%.1f x %.1f x %.1f)\n" % (inndata, str(counter), timestr, Shape_X, Shape_Y, Shape_Z))
-            print("Settings: Clauses: %.1f Threshold: %.1f S: %.1f Window_X: %.1f Window_Y: %.1f\n" % (clauses, Threshold, S, Window_X, Window_Y))
+            print("MultiClassConvolutionalTsetlinMachine2D using %s %s writen to file %s.csv (%.1f x %.1f x %.1f)\n" % (
+            inndata, str(counter), timestr, Shape_X, Shape_Y, Shape_Z))
+            print("Settings: Clauses: %.1f Threshold: %.1f S: %.1f Window_X: %.1f Window_Y: %.1f\n" % (
+            clauses, Threshold, S, Window_X, Window_Y))
 
-        results.write(dim+inndata + numb + ",")
+        results.write(dim + inndata + numb + ",")
         for i in range(epoch):
             start = time()
             m.fit(X_train, Y_train, epochs=1, incremental=True)
@@ -90,44 +121,52 @@ def StartMachine(clauses,epoch,Threshold,S,inndata,dim,machine,Window_X,Window_Y
             start_testing = time()
             result = 100 * (m.predict(X_test) == Y_test).mean()
             stop_testing = time()
-            print("#%d Time: %s Accuracy: %.2f%% Training: %.2fs Testing: %.2fs" % (i + 1, timestamp, result, stop - start,stop_testing-start_testing))
+            print("#%d Time: %s Accuracy: %.2f%% Training: %.2fs Testing: %.2fs" % (
+            i + 1, timestamp, result, stop - start, stop_testing - start_testing))
             results.write(",%.4f" % (np.mean(result)))
         results.write("\n")
         counter += 1
         print("Accuracy:", 100 * (m.predict(X_test) == Y_test).mean())
-        if(counter == 10):
+        if (counter == Write_Clauses):
+            resultclauses = open("Results/" + Name + "/" + machine + "/" + machine + dim + timestr + "clauses1.csv",
+                                 'a')
             PrintClass(m, 1, clauses)
             resultclauses.close()
-            resultclauses = open("Results/"+Name+"/"+machine+"/"+machine+dim + timestr + "clauses0.csv", 'a')
+            resultclauses = open("Results/" + Name + "/" + machine + "/" + machine + dim + timestr + "clauses0.csv",
+                                 'a')
             PrintClass(m, 0, clauses)
             resultclauses.close()
             if (inndata == "draw"):
-                resultclauses = open("Results/"+Name+"/"+machine+"/"+machine+dim + timestr + "clauses2.csv", 'a')
+                resultclauses = open("Results/" + Name + "/" + machine + "/" + machine + dim + timestr + "clauses2.csv",
+                                     'a')
                 PrintClass(m, 2, clauses)
                 resultclauses.close()
-        #Align(tm, 1, clauses)
     print("Prediction: x1 = 1, x2 = 0, ... -> y = %d" % (m.predict(np.array([[1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0]]))))
     print("Prediction: x1 = 0, x2 = 1, ... -> y = %d" % (m.predict(np.array([[0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0]]))))
     print("Prediction: x1 = 0, x2 = 0, ... -> y = %d" % (m.predict(np.array([[0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0]]))))
     print("Prediction: x1 = 1, x2 = 1, ... -> y = %d" % (m.predict(np.array([[1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0]]))))
-    #PrintClass(tm,1,clauses)
-    #resultclauses.close()
-    #resultclauses = open("2Dresults/2D"+timestr+"clauses.csv",'a')
     results.close()
+
+
 def runner():
     # Settings
-    clauses = 100
-    Threshold = 800
+    clauses = 5000
+    Threshold = 80000
     S = 27.0
     epoch = 20
     dim = "9x9"
-    machine = "cTM"    #cTM or TM
-    inndata = "nodraw"
-    Window_X = 4
-    Window_Y = 4
+    machine = "TM"  # cTM or TM
+    # inndata = "Natsukaze_NoDraw"
+    inndata = "Natsukaze_Draw"
+    Window_X = 5
+    Window_Y = 5
     Shape_X = 9
     Shape_Y = 9
     Shape_Z = 2
     Name = "Trond"
-    StartMachine(clauses, epoch, Threshold, S, inndata,dim,machine,Window_X,Window_Y,Shape_X,Shape_Y,Shape_Z,Name)
+    Write_Clauses = 0  # 0 = don't print clauses, 1-10 which k-Fold to write clauses for.
+    StartMachine(clauses, epoch, Threshold, S, inndata, dim, machine, Window_X, Window_Y, Shape_X, Shape_Y, Shape_Z,
+                 Name, Write_Clauses)
+
+
 runner()
