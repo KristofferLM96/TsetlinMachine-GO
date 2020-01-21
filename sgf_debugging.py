4e6ad436ef6915dbb469441ca2573e8b5ffcf845
KristofferLM <k.l.magelssen@gmail.com>
2020-01-21 14:18:07 +0100

    Started working on a check duplication function which will check the dataset for duplicate entries and rewrite file to only have unique entries.

--- sgf_debugging.py
+++ sgf_debugging.py
@@ -1,10 +1,51 @@
-path = "1504341719019999478.sgf"
-file = open(path, 'r', encoding="ISO-8859-1")
-file_lines = file.readlines()
-result = file_lines[0].split("RE")
-_handicap = file_lines[0].split("HA[")
-print(_handicap)
-handicap = _handicap[1][0]
-print(handicap)
+# -----------------------------------------------
+# ................. LIBRARIES ...................
+# -----------------------------------------------
+
+
+import numpy as np
+
+
+# -----------------------------------------------
+# ............. GLOBAL VARIABLES ................
+# -----------------------------------------------
+
+
+# file_name = "9x9Natsukaze_binary.txt"
+# path = "Data/Binary" + file_name
+path = "temp.txt"
+encoding = "UTF-8"  # ISO-8859-1 / UTF-8
+file = open(path, 'r', encoding=encoding)
+unique_list = []
+original_list = []
+
+
+# -----------------------------------------------
+# ................. FUNCTIONS ...................
+# -----------------------------------------------
+
+
+def check_handicap():
+    file_lines = file.readlines()
+    _handicap = file_lines[0].split("HA[")
+    print(_handicap)
+    handicap = _handicap[1][0]
+    print(handicap)
+
+
+def check_duplication():
+    global original_list
+    global unique_list
+    original_list = [line.strip() for line in file]
+    print("Original List Length:", len(original_list))
+    unique_list = np.unique(original_list)
+    print("Unique List Length:", len(unique_list))
+
+
+# -----------------------------------------------
+# .................. MAIN .......................
+# -----------------------------------------------
+# check_handicap()
+check_duplication()
 
 file.close()
