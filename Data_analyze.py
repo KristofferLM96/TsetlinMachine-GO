166a38e36e78c7691d51c75f4587d10091e1cc35
KristofferLM <k.l.magelssen@gmail.com>
2020-01-27 11:27:25 +0100

    Added function to print ratio of win/loss/draw. To run duplicate check on the new dataset require a lot of memory. Had to use jupyter lab.

--- Data_analyze.py
+++ Data_analyze.py
@@ -4,18 +4,19 @@
 
 
 import numpy as np
-import random
+# import random
 
 
 # -----------------------------------------------
 # ............. GLOBAL VARIABLES ................
 # -----------------------------------------------
 
-
-file_name = "9x9Natsukaze_binary.txt"
+# 9x9Natsukaze || 9x9_10k_r104_144x20k
+name = "9x9_10k_r104_144x20k"
+file_name = name + "_binary.txt"
 path = "Data/Binary/" + file_name
 encoding = "UTF-8"  # ISO-8859-1 / UTF-8
-file = open(path, 'r', encoding=encoding)
+multiple_files = True
 unique_list = []
 original_list = []
 
@@ -26,14 +27,18 @@ original_list = []
 
 
 def check_handicap():
+    file = open(path, 'r', encoding=encoding)
     file_lines = file.readlines()
     _handicap = file_lines[0].split("HA[")
     print(_handicap)
     handicap = _handicap[1][0]
     print(handicap)
 
+    file.close()
+
 
 def check_duplication():
+    file = open(path, 'r', encoding=encoding)
     global original_list
     global unique_list
     original_list = [line.strip() for line in file]
@@ -54,11 +59,27 @@ def check_duplication():
     # print("Unique List Length:", unique_length)
     # print("Shuffled - Unique:", shuffled_length - unique_length, "\n")
 
+    file.close()
+
+
+def get_result_ratio():
+    win = open("Data/Results-Split/" + name + "_win.txt", 'r')
+    loss = open("Data/Results-Split/" + name + "_loss.txt", 'r')
+    draw = open("Data/Results-Split/" + name + "_draw.txt", 'r')
+    win_line = win.readlines()
+    loss_line = loss.readlines()
+    draw_line = draw.readlines()
+    print("Amount of wins: ", len(win_line))
+    print("Amount of loss: ", len(loss_line))
+    print("Amount of draw: ", len(draw_line))
+    win.close()
+    loss.close()
+    draw.close()
+
 
 # -----------------------------------------------
 # .................. MAIN .......................
 # -----------------------------------------------
 # check_handicap()
-check_duplication()
-
-file.close()
+# check_duplication()
+get_result_ratio()
